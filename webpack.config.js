b31869e4570988b0a84cfd38e88e5c1cde1fa3f1
Andrew Fetisov <afetisov@magecore.com>
2019-05-20 22:55:34 +0300

    - working example with dependencies,configs and lock files

--- webpack.config.js
+++ webpack.config.js
@@ -17,12 +17,12 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
-    .addEntry('app', './assets/js/app.js')
+    // .addEntry('app', './assets/js/app.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
-    .splitEntryChunks()
+    // .splitEntryChunks()
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
