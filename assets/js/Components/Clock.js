3c9de843b4ac778dc6bf3b4f110ade302a1cc0ca
Andrew Fetisov <afetisov@magecore.com>
2019-06-03 13:15:56 +0300

    - Clock, Serarch example components - Styling

---
+++ assets/js/Components/Clock.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+
+class Clock extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            date: new Date()
+        };
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        let helloWorld = 'Hello Ukraine';
+        return (
+            <div>
+                <h1>{helloWorld}</h1>
+                <h2>Kiev time is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
+}
+
+export default Clock;
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,43 +1,152 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import '../css/app.css';
 
-import Items from './Components/Items';
+import Clock from './Components/Clock';
 
-class App extends React.Component {
+const list = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: 'Redux',
+        url: 'https://redux.js.org/',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    }
+
+];
+
+function isSearched(searchTerm) {
+    return function (item) {
+        return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+    }
+}
+
+class Search extends Component {
+    render() {
+        const {value, onChange} = this.props;
+        return (
+            <form>
+                <input
+                    type="text"
+                    value={value}
+                    onChange={onChange}
+                />
+            </form>
+         );
+    }
+}
+
+class Table extends Component {
+    render() {
+        const { list, pattern, onDismiss } = this.props;
+        return (
+            <div>
+            {list.filter(isSearched(pattern)).map(item =>
+                    <div key={item.objectID}>
+                        <span>
+                            <a href={item.url}>{item.title}</a>
+                        </span>
+                        <span>{item.author}</span>
+                        <span>{item.num_comments}</span>
+                        <span>{item.points}</span>
+                        <span>
+                            <Button onClick={() => onDismiss(item.objectID)}>
+                                Dismiss
+                            </Button>
+                        </span>
+                    </div>
+                )}
+            </div>
+        );
+    }
+
+}
+
+class Button extends Component {
+    render() {
+        const {
+            onClick,
+            className = '',
+            children
+        } = this.props;
+        return (
+            <button
+                onClick={onClick}
+                className={className}
+                type="button"
+            >
+                {children}
+            </button>
+        );
+    }
+}
+
+class List extends Component {
+
     constructor(props) {
         super(props);
 
         this.state = {
-            entries: []
+            list,
+            searchTerm: '',
         };
+
+        this.onDismiss = this.onDismiss.bind(this);
+        this.onSearchChange = this.onSearchChange.bind(this);
     }
 
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/posts/')
-            .then(response => response.json())
-            .then(entries => {
-                this.setState({
-                    entries
-                });
-            });
+    onSearchChange(event) {
+        this.setState({ searchTerm: event.target.value });
+    }
+
+    onDismiss(id) {
+        function isNotId(item) {
+            return item.ObjectID !== id;
+        }
+        const updatedList = this.state.list.filter(isNotId);
+        this.setState({ list: updatedList });
     }
 
     render() {
+        const {searchTerm, list} = this.state;
+
         return (
-            <div className="row">
-                {this.state.entries.map(
-                    ({ id, title, body }) => (
-                        <Items
-                            key={id}
-                            title={title}
-                            body={body}
-                        >
-                        </Items>
-                    )
-                )}
+            <div className="page">
+                <div className="interactions">
+                    <Search
+                        value={searchTerm}
+                        onChange={this.onSearchChange}
+                    />
+                    <Table
+                        list={list}
+                        pattern={searchTerm}
+                        onDismiss={this.onDismiss}
+                    />
+                </div>
             </div>
         );
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function App() {
+    return (
+        <div>
+            <Clock/>
+            <List/>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root'))
+;
